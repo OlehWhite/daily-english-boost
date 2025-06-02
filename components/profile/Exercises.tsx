96fda660525dd25f38ecf7fc690615695d719a22
Bilostotskyi-Oleh <oleh.bilostotskyi@gointeractive.co>
2025-06-02 18:03:33 +0300

    test commit from OlehWhite account

--- components/profile/Exercises.tsx
+++ components/profile/Exercises.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const MotionLink = motion(Link);
+const MotionLink = motion.create(Link);
 
 const Exercises = () => {
   const { ref, isInView } = useMotionWithDelay<HTMLDivElement>();
